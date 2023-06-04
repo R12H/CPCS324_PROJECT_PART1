d811a3ed75498619eb947bf0836bbc67b732faca
wedaljahdali <132339188+wedaljahdali@users.noreply.github.com>
2023-06-04 21:38:18 +0300

    Update Edge.java

--- Graph Framework/Edge.java
+++ Graph Framework/Edge.java
@@ -1,4 +1,15 @@
+/* Team members:
+Raghad Hawsawi 2105869 - Najd Khalid 2006156 - Wed Aljahdali 2105502 - Renad Baghdadi 2006538
 
+Resources
+1)  Levitin, A. (2012). Introduction to the design and analysis of algorithms. Pearson.
+2)  KruskalAlg: https://algorithms.tutorialhorizon.com/kruskals-algorithm-minimum-spanning-tree-mst-complete- java-implementation/
+3)  MinHeap:  https://medium.com/@ankur.singh4012/implementing-min-heap-in-java-413d1c20f90d
+4)  MHPrimAlg :  https://www.baeldung.com/java-prim-algorithm
+
+Section:
+CP1
+ */
 package GraphFramework;
 
 import java.util.*;
@@ -52,8 +63,6 @@ public class Edge implements Comparator<Edge> {
     }
     
     public void displayInfo() {
-        //return "Office No. "+ this.source.getLabel()+" - " + "Office No."+ this.target.getLabel() + " : line length: "+ weight;
-        System.out.print(" line length: "+ weight);
     }
     
 }
