5d6e82d00487d0f194824a90e5f194c50ec4cf0a
wedaljahdali <132339188+wedaljahdali@users.noreply.github.com>
2023-06-04 21:37:25 +0300

    Update MinHeap.java

--- Graph Framework/MinHeap.java
+++ Graph Framework/MinHeap.java
@@ -1,16 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Team members:
+Raghad Hawsawi 2105869 - Najd Khalid 2006156 - Wed Aljahdali 2105502 - Renad Baghdadi 2006538
+
+Resources
+1)  Levitin, A. (2012). Introduction to the design and analysis of algorithms. Pearson.
+2)  KruskalAlg: https://algorithms.tutorialhorizon.com/kruskals-algorithm-minimum-spanning-tree-mst-complete- java-implementation/
+3)  MinHeap:  https://medium.com/@ankur.singh4012/implementing-min-heap-in-java-413d1c20f90d
+4)  MHPrimAlg :  https://www.baeldung.com/java-prim-algorithm
+
+Section:
+CP1
  */
 package GraphFramework;
 
 import java.util.Arrays;
 
-/**
- *
- * @author Raghad
- */
 public class MinHeap {
 
   private int capacity;
@@ -22,49 +25,59 @@ public class MinHeap {
     this.heap = new Edge[capacity];
   }
 
-
+//---------------------------------------------------------------------------------- 
   public int getLeftChildIndex(int parentIndex) {
     return 2 * parentIndex + 1;
   }
 
+  //----------------------------------------------------------------------------------
   public int getRightChildIndex(int parentIndex) {
     return 2 * parentIndex + 2;
   }
 
+  //----------------------------------------------------------------------------------
   public int getParentIndex(int childIndex) {
     return (childIndex - 1) / 2;
   }
 
+  //----------------------------------------------------------------------------------
   public boolean hasLeftChild(int index) {
     return getLeftChildIndex(index) < size;
   }
 
+  //----------------------------------------------------------------------------------
   public boolean hasRightChild(int index) {
     return getRightChildIndex(index) < size;
   }
 
+  //----------------------------------------------------------------------------------
   public boolean hasParent(int index) {
     return getParentIndex(index) >= 0;
   }
 
+  //----------------------------------------------------------------------------------
   public Edge leftChild(int parentIndex) {
     return heap[getLeftChildIndex(parentIndex)];
   }
 
+  //----------------------------------------------------------------------------------
   public Edge rightChild(int parentIndex) {
     return heap[getRightChildIndex(parentIndex)];
   }
 
+  //----------------------------------------------------------------------------------
   public Edge parent(int childIndex) {
     return heap[getParentIndex(childIndex)];
   }
 
+  //----------------------------------------------------------------------------------
   public void swap(int index1, int index2) {
     Edge element = heap[index1];
     heap[index1] = heap[index2];
     heap[index2] = element;
   }
 
+  //----------------------------------------------------------------------------------
   private void ensureCapacity() {
     if (size == capacity) {
       heap = Arrays.copyOf(heap, capacity * 2);
@@ -72,6 +85,7 @@ public class MinHeap {
     }
   }
 
+  //----------------------------------------------------------------------------------
   // Time Complexity : O(1)
   public Edge peek() {
     if (size == 0) {
@@ -80,6 +94,7 @@ public class MinHeap {
     return heap[0];
   }
 
+  //----------------------------------------------------------------------------------
   // Time Complexity : O( Log n)
   public Edge poll() {
     if (size == 0) {
@@ -94,6 +109,7 @@ public class MinHeap {
     return element;
   }
 
+  //----------------------------------------------------------------------------------
   // Time Complexity : O( Log n)
   public void add(Edge item) {
     ensureCapacity();
@@ -102,6 +118,7 @@ public class MinHeap {
     heapifyUp();
   }
 
+  //----------------------------------------------------------------------------------
   private void heapifyUp() {
     int index = size - 1;
 
@@ -111,6 +128,7 @@ public class MinHeap {
     }
   }
 
+  //----------------------------------------------------------------------------------
   private void heapifyDown() {
     int index = 0;
 
@@ -130,6 +148,7 @@ public class MinHeap {
     }
   }
 
+  //----------------------------------------------------------------------------------
   public void printHeap() {
     for (int i = 0; i < size; i++) {
       System.out.print(heap[i] + " ");
