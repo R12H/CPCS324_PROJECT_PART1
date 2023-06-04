6dc61689183cd50375bb83c760c2ce581f05b9e3
wedaljahdali <132339188+wedaljahdali@users.noreply.github.com>
2023-06-04 21:34:32 +0300

    Update Vertex.java

--- Graph Framework/Vertex.java
+++ Graph Framework/Vertex.java
@@ -1,3 +1,15 @@
+/* Team members:
+Raghad Hawsawi 2105869 - Najd Khalid 2006156 - Wed Aljahdali 2105502 - Renad Baghdadi 2006538
+
+Resources
+1)  Levitin, A. (2012). Introduction to the design and analysis of algorithms. Pearson.
+2)  KruskalAlg: https://algorithms.tutorialhorizon.com/kruskals-algorithm-minimum-spanning-tree-mst-complete- java-implementation/
+3)  MinHeap:  https://medium.com/@ankur.singh4012/implementing-min-heap-in-java-413d1c20f90d
+4)  MHPrimAlg :  https://www.baeldung.com/java-prim-algorithm
+
+Section:
+CP1
+ */
 
 package GraphFramework;
 
